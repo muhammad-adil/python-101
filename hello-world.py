7743337bea494e0d0df5dca91df679dc593a5b45
Muhammad-Adil <adil.muhammad93@gmail.com>
2018-11-16 22:50:34 +0100

    python intro

---
+++ hello-world.py
@@ -0,0 +1,35 @@
+
+# Python 101
+
+# print("Hello, World!") 
+
+# Python uses indentation to indicate a block of code.
+if 5 > 2:
+  print("Five is greater than two!")
+
+
+# x = 1    # int
+# y = 2.8  # float
+# z = 1j   # complex
+
+# print(type(x))
+# print(type(y))
+# print(type(z))
+
+
+# Integers
+x = int(1)   # x will be 1
+y = int(2.8) # y will be 2
+z = int("3") # z will be 3
+
+# Floats
+x = float(1)     # x will be 1.0
+y = float(2.8)   # y will be 2.8
+z = float("3")   # z will be 3.0
+w = float("4.2") # w will be 4.2
+
+
+# Strings
+x = str("s1") # x will be 's1'
+y = str(2)    # y will be '2'
+z = str(3.0)  # z will be '3.0' 
