162ee37237c8a52e2767fb2db597133dbfd7b0fc
Muhammad-Adil <adil.muhammad93@gmail.com>
2018-11-17 21:44:27 +0100

    python strings

--- hello-world.py
+++ hello-world.py
@@ -33,3 +33,9 @@ w = float("4.2") # w will be 4.2
 x = str("s1") # x will be 's1'
 y = str(2)    # y will be '2'
 z = str(3.0)  # z will be '3.0' 
+
+
+# // String
+a = "Hello, World!"
+print(a[1])
+# e
