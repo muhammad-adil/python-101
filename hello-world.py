2ab24211681630d11e606cdaeb3a68f32d4561ae
Muhammad-Adil <adil.muhammad93@gmail.com>
2018-11-17 21:52:45 +0100

    python strings

--- hello-world.py
+++ hello-world.py
@@ -46,5 +46,31 @@ print(b[2:5])
 # llo
 
 abc = " Hello, World! "
-print(a.strip()) 
+print(abc.strip()) 
 # "Hello, World!" 
+
+
+# The len() method returns the length of a string:
+hello = "Hello, World!"
+print(len(hello))
+# output is 13
+
+# The lower() method returns the string in lower case:
+hw = "Hello, World!"
+print(hw.lower())
+# output is  hello, world! 
+
+
+# The upper() method returns the string in upper case:
+e = "Hello, World!"
+print(e.upper())
+# output is  HELLO, WORLD! 
+
+# The replace() method replaces a string with another string:
+f = "Hello, World!"
+print(f.replace("H", "J"))
+# Jello, World! 
+
+
+
+
