590846c45e878a977d814c375d5caf003b998a54
Muhammad-Adil <adil.muhammad93@gmail.com>
2018-11-19 01:01:12 +0100

    python string

--- 01-variables.py
+++ 01-variables.py
@@ -34,3 +34,10 @@ print(type(yc))
 print(type(zc))
 
 
+# Casting in python is therefore done using constructor functions:
+  # int() - constructs an integer number from an integer literal, a float literal (by rounding down to the previous whole number), or a string literal (providing the string represents a whole number)
+  
+  # float() - constructs a float number from an integer literal, a float literal or a string literal (providing the string represents a float or an integer)
+  
+  # str() - constructs a string from a wide variety of data types, including strings, integer literals and float literals
+
---
+++ 02-strings.py
@@ -0,0 +1,45 @@
+
+# Python 101
+
+
+# // String
+a = "Hello, World!"
+print(a[1])
+# output is 
+# e
+
+b = "Hello, World!"
+print(b[2:5])
+# output is 
+# llo
+
+abc = " Hello, World! "
+print(abc.strip()) 
+# "Hello, World!" 
+
+# The len() method returns the length of a string:
+hello = "Hello, World!"
+print(len(hello))
+# output is 13
+
+# The lower() method returns the string in lower case:
+hw = "Hello, World!"
+print(hw.lower())
+# output is  hello, world! 
+
+# The upper() method returns the string in upper case:
+e = "Hello, World!"
+print(e.upper())
+# output is  HELLO, WORLD! 
+
+# The replace() method replaces a string with another string:
+f = "Hello, World!"
+print(f.replace("H", "J"))
+# Jello, World! 
+
+# The split() method splits the string into substrings if it finds instances of the separator:
+g = "Hello, World!"
+print(g.split(",")) 
+# returns ['Hello', ' World!'] 
+
+
